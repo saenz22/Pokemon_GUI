e307feed6ae1d9690a1366877be0a9486f6434ed
saenz22 <saenz.sebastian@correounivalle.edu.co>
2025-05-01 12:07:43 -0500

    Se adaptó el código para terminar la interacción con el usuario adecuadamente y obtener una estructura más limpia y legible. Algunos comentarios explicativos fueron dejados en clase Batalla. Se creó interfaz interaccionUsuario, que gestionará la parte audiovisual

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        /* 
         // Se inicializan 2 entrenadores
-        Entrenador e1 = Entrenador.capturarEntrenador();
-        Entrenador e2 = Entrenador.capturarEntrenador();
+        Entrenador e1 = Entrenador.capturarEntrenador(nombre, pokemon1, pokemon2, pokemon3);
+        Entrenador e2 = Entrenador.capturarEntrenador(nombre, pokemon1, pokemon2, pokemon3);
         
+
         // Se inializa la batalla
-        Batalla.batallaPorEquipos(e1, e2);
+        Batalla.batallaPorEquipos(e1, eleccion1, e2, eleccion2);
+        */
     }
 }
---
+++ interaccionUsuario.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList; 
+public interface interaccionUsuario {
+    
+    public static Ataque elegirAtaque(Pokemon atacante) {
+        return null;
+    }
+
+    public static Pokemon elegirNuevoPokemon(Entrenador entrenador, ArrayList<Pokemon> disponibles) {
+        return null;
+    }
+
+    // Para completar el MVC, necesitamos un método que obtenga entradas de usuario, tanto para GUI como para terminal
+}
